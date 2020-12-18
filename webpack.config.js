380cc3c5c1c12d6981eb20dfe630036f6c5c7086
oyebodefreeman <bodokunfolami@gmail.com>
2020-12-18 16:25:43 +0100

    added webpack-dev-server module

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
@@ -17,6 +18,12 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, './public/index.html'),
+        })
+    ]
    
 }
